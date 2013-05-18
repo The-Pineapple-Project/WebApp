3b2d409848739e5e2108ffcc592ab141fd345988
Bill Mei <kortaggio@gmail.com>
2013-05-18 17:42:50 -0400

    rotating pineapple

--- SplashPage/js/main.js
+++ SplashPage/js/main.js
@@ -1,1 +1,25 @@
+var $pineapple = $('#logo-left');
+var start_angle = 0;
+var global_timer;
 
+$('#logo').mouseenter(function () {
+    rotate($pineapple, start_angle);
+}).mouseleave(function () {
+    stop_rotation();
+});
+
+
+function rotate (el, angle) {
+    el.css({
+        transform: 'rotate(' + angle + 'deg)'
+    });
+
+    global_timer = setTimeout(function () {
+        start_angle = ++angle;
+        rotate(el, angle, global_timer);
+    },25);
+}
+
+function stop_rotation () {
+    window.clearInterval(global_timer);
+}
